495779334f4808c4aa2719ec8cba0acd3e63c6f4
Fabian Nobert <fabiannobert.007@gmail.com>
2025-07-19 16:18:07 +0100

    update login form UX and localstorage login

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+  const emailField = document.getElementById("Email-input");
+  const passwordField = document.getElementById("password-input");
+  const errorMsg = document.getElementById("error-msg");
+  const loginBtn = document.getElementById("login-btn");
 
-const loginForm = document.getElementById("login-form")
-const email = document.getElementById("Email-input")
-const password = document.getElementById("password-input")
-const loginBtn = document.getElementById("login-btn")
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    errorMsg.textContent = "";
+    errorMsg.classList.add("hidden");
 
+    const email = emailField.value.trim();
+    const password = passwordField.value.trim();
 
-loginForm.addEventListener("submit", function(e){
-   e.preventDefault()
-   const Email = email.value.trim()
-   const Password = password.value.trim()
-   console.log(Email, Password)
-   const user = {email, password}
-   localStorage.setItem("helloDayUser",  JSON.stringify(user))
-   console.log(user)
-})
-})
+    if (email === "" || password === "") {
+      errorMsg.textContent = "Please fill in both fields.";
+      errorMsg.classList.remove("hidden");
+      errorMsg.style.color = "red";
+
+      return;
+    }
+    const user = { email, password };
+    login(user);
+    // console.log(email, password)
+    // console.log(user)
+  });
+
+  function login(user) {
+    const storedUser = JSON.parse(localStorage.getItem("helloDayUser"));
+    // console.log(typeof storedUser)
+    // console.log(storedUser)
+
+    if (!storedUser) {
+      errorMsg.textContent = "No user found. Please sign up first";
+      errorMsg.classList.remove("hidden");
+      errorMsg.style.color = "red";
+      return;
+    } else if (
+      user.email === storedUser.email &&
+      user.password === storedUser.password
+    ) {
+      localStorage.setItem("isLoggedIn", "true");
+      window.location.href = "weather.html";
+    } else {
+      errorMsg.textContent = "invalid email or password";
+      errorMsg.classList.remove("hidden");
+      errorMsg.style.color = "red";
+      return;
+    }
+  }
+});
